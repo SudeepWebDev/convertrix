35dbf7f43289df612b241771f3c5933602e8110d
SudeepWebDev <127820890+SudeepWebDev@users.noreply.github.com>
2023-07-17 03:05:05 +0530

    Update chat.js

--- JAVASCRIPT/chat.js
+++ JAVASCRIPT/chat.js
@@ -595,15 +595,45 @@ function convertArea(convertOption1, convertOption2, amount) {
 }
 function convertLength(convertOption1, convertOption2, amount) {
     const lengthValueM = {
+        // Metric Units
         'm': 1,
         'mm': 1000,
         'cm': 100,
         'km': 0.001,
-        'in': 39.3701,
-        'ft': 3.28084,
-        'yd': 1.09361,
-        'mi': 0.000621
-    };
+        'µm': 1e+6,        // Micrometer (µm)
+        'nm': 1e+9,        // Nanometer (nm)
+        'zm': 1e+21,       // Zeptometer (zm)
+        'am': 1e+18,       // Attometer (am)
+        'fm': 1e+15,       // Femtometer (fm)
+        'pm': 1e+12,       // Picometer (pm)
+        'dm': 10,        // Decimeter (dm)
+        'dam': 0.1,         // Decameter (dam)
+        'hm': 1e-2,         // Hectometer (hm)
+        'Mm': 1e-6,         // Megameter (Mm)
+        'Gm': 1e-9,         // Gigameter (Gm)
+        'Tm': 1e-12,        // Terameter (Tm)
+        'Pm': 1e-15,        // Petameter (Pm)
+        'Em': 1e-18,        // Exameter (Em)
+        'Zm': 1e-21,        // Zettameter (Zm)
+        'Ym': 1e-24,        // Yottameter (Ym)
+      
+        // Imperial Units
+        'in': 39.3700787,      // Inch (in)
+        'ft': 3.2808399,      // Feet (ft)
+        'yd': 1.0936133,      // Yard (yd)
+        'mi': 0.00062137,     // Mile (mi)
+        'nmi': 0.00053996,       // Nautical Mile (nmi)
+        'furlong': 0.00497097, // Furlong (furlong)
+        'fathom': 0.54680665,  // Fathom (fathom)
+      
+        // Astronomical Units
+        'AU': 6.68458712E-12, // Astronomical Unit (AU)
+        'ly': 1.05700083E-16,    // Light Year (ly)
+        'pc': 3.24078E-17,    // Parsec (pc)
+        
+        // Lunar Distances
+        'LD': 2.604166E-9 ,   // Lunar Distance (LD)
+      };
 
     let convertedAmount;
     let convertedLengthM;
@@ -611,13 +641,13 @@ function convertLength(convertOption1, convertOption2, amount) {
     for (keys in lengthValueM) {
         if (keys == convertOption2) {
             if (convertOption1 == 'm') {
-                convertedAmount = Number(amount) * lengthValueM[keys];
+                convertedAmount = Number(amount) * lengthValueM[keys.toLowerCase()];
                 return convertedAmount;
                 //   break;
             } else {
                 for (keys2 in lengthValueM) {
                     if (keys2 == convertOption1) {
-                        convertedLengthM = Number(amount) / lengthValueM[keys2];
+                        convertedLengthM = Number(amount) / lengthValueM[keys2.toLowerCase()];
                         convertedAmount = convertedLengthM * lengthValueM[convertOption2];
                         return convertedAmount;
                         //    break;
